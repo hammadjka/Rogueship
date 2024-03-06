a8a349e06a9c6970d2a96384d1f9234e662f36bc
Hammad <javedhammad034@gmail.com>
2024-03-06 08:38:49 -0800

    added sounds for characters, selection screen complete

--- src/controller.js
+++ src/controller.js
@@ -1,34 +1,35 @@
 import './styles.css'
 import View from './view.js'
-import menuMusic from './assets/sounds/Menu.wav'
 
 const Controller = (function() {
-    const bgMusic = new Audio(menuMusic);
 
-    const toggleBgMusic = (button)=>{
-        if(button.classList.contains("off")){
-            bgMusic.loop = true;
-            bgMusic.play();
-            View.toggleVolImg();
-            button.classList = [];
-            button.classList.add("on");
-        }else{
-            bgMusic.pause();
-            // bgMusic.currentTime = 0;  
-            View.toggleVolImg();
-            button.classList = [];
-            button.classList.add("off");
-        }
-        
-    }
-    return{toggleBgMusic}
 })();
 
 document.addEventListener('DOMContentLoaded', function() {
-    let flagGif = document.getElementById('flag');
-    let volumeButton = document.getElementById('volBtn');
-    volumeButton.addEventListener('click', (e)=>{
-        Controller.toggleBgMusic(e.target);
-    })
+    const flagGif = document.getElementById('flag');
     View.fadeIn(flagGif, 500);
+
+    const volumeButton = document.getElementById('volBtn');
+    volumeButton.addEventListener('click', (e)=>{
+        View.toggleMenuMusic(e.target);
+    })
+
+    const characterButtons = document.querySelectorAll(".select");
+    characterButtons.forEach(button => {
+        button.addEventListener('click', (e)=>{
+            View.selectCharacter(e.target);
+        })
+    })
+
+    const closeModalBtn = document.querySelector("#closeModal");
+    closeModalBtn.addEventListener('click', View.resetSelectScreen);
+
+    const modal = document.querySelector('#modal');
+    modal.addEventListener('click', function(event) {
+        let isClickInsideModal = document.querySelector('.modal-content').contains(event.target);
+        if (!isClickInsideModal) {
+           View.resetSelectScreen();
+        }
+    });
+
 });
--- src/view.js
+++ src/view.js
@@ -1,8 +1,93 @@
 
 import volumeOn from './assets/menu/volume-high.svg';
 import volumeOff from './assets/menu/volume-off.svg';
+import menuMusic from './assets/sounds/GameMusic/Menu.wav'
+import viking1 from './assets/sounds/VikingDialogue/sheildTosheild.wav'
+import viking2 from './assets/sounds/VikingDialogue/thorJudge.wav'
+import viking3 from './assets/sounds/VikingDialogue/valhallaAdmits.wav'
+import viking4 from './assets/sounds/VikingDialogue/leaveMe.wav'
+
+import pirate1 from './assets/sounds/PirateDialogue/allHands1.wav'
+import pirate2 from './assets/sounds/PirateDialogue/hoistJelly.wav'
+import pirate3 from './assets/sounds/PirateDialogue/meRum.wav'
+import pirate4 from './assets/sounds/PirateDialogue/allHands2.wav'
+
+const GameAudio = (function(){
+    const menu = new Audio(menuMusic);
+    menu.volume = 0.7;
+
+    const V_SelectD1 = new Audio(viking1);
+    const V_SelectD2 = new Audio(viking2);
+    const V_SelectD3 = new Audio(viking3);
+    const V_SelectD4 = new Audio(viking4);
+
+    const P_SelectD1 = new Audio(pirate1);
+    const P_SelectD2 = new Audio(pirate2);
+    const P_SelectD3 = new Audio(pirate3);
+    const P_SelectD4 = new Audio(pirate4);
+
+
+
+    const bgMusic = {menu};
+    const dialogueObj = {
+                        "viking": {V_SelectD1, V_SelectD2, V_SelectD3, V_SelectD4},
+                        "pirate": {P_SelectD1, P_SelectD2, P_SelectD3, P_SelectD4}
+    }
+
+    const playSelectDialogue = (character)=>{
+        stopAllDialogues();
+        let audioName;
+        for(let ch in dialogueObj){
+            if (ch === character){
+                const audios = Object.keys(dialogueObj[ch]);
+                const randomIndex = Math.floor(Math.random() * audios.length);
+                audioName = audios[randomIndex];
+            }
+        }
+        let dialogue =  dialogueObj[character][audioName];
+        menu.volume = 0.35;
+        dialogue.play();
+        dialogue.addEventListener('ended', function() {
+            menu.volume = 0.7;
+        });
+    }
+    const stopDialogue = (character, audioName)=>{
+        dialogueObj[character][audioName].pause();
+        dialogueObj[character][audioName].currentTime = 0;
+    }
+    const stopAllDialogues = ()=>{
+        for (let character in dialogueObj) {
+            for(let dialogue in dialogueObj[character]){
+                stopDialogue(character, dialogue);
+            }
+        }
+        menu.volume = 0.7;    
+    }
+    const playBgMusic = (audioName)=>{
+        bgMusic[audioName].loop = true;
+        bgMusic[audioName].play();
+    }
+    const stopBgMusic = (audioName)=>{
+        bgMusic[audioName].pause();
+    }
+    return{playSelectDialogue, playBgMusic, stopBgMusic, stopAllDialogues, stopDialogue}
+})();
 
 const View = (function() {
+    const toggleMenuMusic = (button)=>{
+        let img = document.querySelector("#volImg");
+        if(button.classList.contains("off")){
+            GameAudio.playBgMusic("menu");
+            img.src = volumeOn;
+            button.classList = [];
+            button.classList.add("on");
+        }else{
+            GameAudio.stopBgMusic("menu");
+            img.src = volumeOff;
+            button.classList = [];
+            button.classList.add("off");
+        }
+    }
     const fadeIn = (element, duration) => {
         var opacity = 0;
         var interval = 50; // Interval in milliseconds
@@ -17,19 +102,25 @@ const View = (function() {
             }
         }, interval);
     }
-    const toggleVolImg = ()=>{
-        let img = document.querySelector("#volImg");
-        if (img.classList.contains("off")){
-            img.classList = [];
-            img.classList.add("on");
-            img.src = volumeOn;
-        }else{
-            img.classList = [];
-            img.classList.add("off");
-            img.src = volumeOff;
+    const resetSelectScreen = ()=>{
+        let characterButtons = document.querySelectorAll(".select");
+        characterButtons.forEach(button => {
+            button.style.backgroundColor = "inherit";
+        });
+        document.querySelector("#readyButton").classList = [];
+        GameAudio.stopAllDialogues();
+    }
+    const selectCharacter = (character)=>{
+        resetSelectScreen();
+        character.style.backgroundColor = "#E3E3E3";
+        document.querySelector("#readyButton").classList.add("ready");
+        if(character.id == "selectViking"){
+            GameAudio.playSelectDialogue("viking");
+        }else if(character.id == "selectPirate"){
+            GameAudio.playSelectDialogue("pirate");
         }
     }
-    return {fadeIn, toggleVolImg}
+    return {fadeIn, toggleMenuMusic, selectCharacter, resetSelectScreen}
 })();
 
 export default View;
